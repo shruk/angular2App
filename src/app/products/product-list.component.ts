2c825026119c4fc2c2bf9a694740154f3db7495f
Yongfang <windowsyyf@gmail.com>
2019-01-29 21:49:18 -0500

    update

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -2,9 +2,36 @@ import { Component } from '@angular/core';
 
 @Component({
 selector:'app-products',
-templateUrl:'./product-list.componnent.html'
+templateUrl:'./product-list.component.html'
 })
 export class ProductListComponent{
     pageTitle:string='Product List';
-
+    imageWidth:number=50;
+    imageMargin:number=2;
+    showImage:boolean=false;
+    listFilter:string='cart';
+//need to populate some product infomation.
+products:any[]=[
+    {"productId":1,
+    "productName":"Garden Cart",
+    "productCode":"GDN-0023",
+    "releaseDate":"March 18, 2016",
+    "description":"15 gallon capacity rolling garden",
+    "price":32.99,
+    "starRating":4.2,
+    "imageUrl":"https://openclipart.org/image/300px/svg"
+    },
+    {"productId":2,
+    "productName":"Hammer",
+    "productCode":"TBX-0048",
+    "releaseDate":"May 21, 2016",
+    "description":"Curved claw steel hammer",
+    "price":8.9,
+    "starRating":4.8,
+    "imageUrl":"https://openclipart.org/image/300px/svg"
+    }
+    ];
+    toggleImage():void{
+        this.showImage=!this.showImage;
+    }
 }
