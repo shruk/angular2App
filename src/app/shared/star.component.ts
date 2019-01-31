6dd2a555d9b5aa766107eaa3792cf461bfffe2f6
phillip <windowsyyf@gmail.com>
2019-01-31 15:12:08 -0500

    create data services

--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnChanges, Input } from '@angular/core';
+import { Component, OnChanges, Input, Output,EventEmitter } from '@angular/core';
+//import { EventEmitter } from 'protractor';
 
 @Component({
   selector:'star',
@@ -10,12 +11,16 @@ export class StarComponnent implements OnChanges
      starWidth:number;
     @Input() rating:number; //this value comes from container component
 
-
-    constructor(){
-          }
+    //output describe event raised to container
+    @Output() notify: EventEmitter<string>
+    =new EventEmitter<string>();
 
     ngOnChanges():void{
 
       this.starWidth=(this.rating*75/5);
+    }
+
+    onClick():void{
+      this.notify.emit(`The rating ${this.rating} was clicked!`);
     }
 }
