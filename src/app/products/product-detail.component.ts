d2b38bd934991c1178917d3ef450a70283721255
phillip <windowsyyf@gmail.com>
2019-02-01 22:04:39 -0500

    add back button

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -10,12 +10,12 @@ export class ProductDetailComponent implements OnInit {
 
   pageTitle:string='Product Detail';
   product: IProduct;
-  constructor(private route:ActivatedRoute) {
+  constructor(private route:ActivatedRoute,private router:Router) {
    }
 
   ngOnInit() {
-    let id=+this.route.snapshot.paramMap.get('id');
-    this.pageTitle+=`: $(id)`;
+    let id=+this.route.snapshot.paramMap.get('id');//plus sign convert string to number
+    this.pageTitle += " : "+id;
     this.product={
       "productId": id,
       "productName": "Leaf Rake",
@@ -27,6 +27,10 @@ export class ProductDetailComponent implements OnInit {
       "imageUrl": "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
 
     }
+  }
+
+  onBack():void{
+    this.router.navigate(['/products']);
   }
 
 }
